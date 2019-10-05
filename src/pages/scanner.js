58aa4d0e4578e9d3a0ff6d59607a61ff5db48f85
Andreas Moldskred <moldskredandreas1@gmail.com>
2019-10-05 23:39:11 +0200

    Fix QR-reader and make detect if it should use legacy-functions

--- src/pages/scanner.js
+++ src/pages/scanner.js
@@ -1,10 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import { Router } from "@reach/router"
 import { login, logout, isAuthenticated, getProfile } from "../utils/auth"
 import { Link } from "gatsby"
+import QrReader from "react-qr-scanner"
 
-const Home = ({ user }) => {
-  return <p>Hi, {user.name ? user.name : "friend"}!</p>
+const QRScanner = ({ user }) => {
+  const [code, setCode] = useState() // The const code will be the the qr code
+  const noSupport =
+    !navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices
+  return (
+    <QrReader
+      style={{
+        width: "100vw",
+        height: "100vh",
+        maxWidth: "100vw",
+        maxHeight: "100vh",
+        position: "fixed",
+        background: "rgba(0,0,0,0.8)",
+        objectFit: "cover",
+        zIndex: "-1",
+        top: 0,
+        left: 0,
+      }}
+      lagecyMode={noSupport}
+      openImageDialog={noSupport && !code}
+      onError={err => console.error(err)}
+      onImageLoad={() => {
+        console.log("loaded")
+      }}
+      onScan={code => {
+        if (!code) return
+        setCode(code)
+      }}
+    />
+  )
 }
 
 const Scanner = () => {
@@ -30,7 +59,7 @@ const Scanner = () => {
         </a>
       </nav>
       <Router>
-        <Home path="/scanner/" user={user} />
+        <QRScanner path="/scanner/" user={user} />
       </Router>
     </>
   )
